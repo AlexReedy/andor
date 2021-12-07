6d22dd10cd4b551a1d33523927260a2c8b9d3002
AlexReedy <alexander.h.reedy@gmail.com>
2021-12-07 10:52:27 -0800

    Adding to script to get all necessary info from the camera to compare to the pixis parameters

--- camera_info_getter.py
+++ camera_info_getter.py
@@ -1,28 +1,50 @@
 import platform
 import time
 from andorLib import *
-from astropy.io import fits
+import pandas as pd
+
+# from astropy.io import fits
 
 andor = Andor()
 andor.loadLibrary()
 
-connected_cams = andor.GetAvailableCameras()[1]
+connected_cams = (andor.GetAvailableCameras()[1]-1)
+print(f'Number of Connected Cameras: {connected_cams}')
 
-handle = andor.GetCameraHandle(connected_cams[1])
+handle = andor.GetCameraHandle(connected_cams)[1]
+print(f'Camera Handle: {handle}')
+
 andor.SetCurrentCamera(handle[1])
-
-initialize = andor.Initialize()
+andor.Initialize()
 
 serial = andor.GetCameraSerialNumber()[1]
+print(f'Camera Serial Number: {serial}')
+
 detector_dim = [andor.GetDetector()[1], andor.GetDetector()[2]]
+print(f'Detector Dimensions: [{detector_dim[0]}, {detector_dim[1]}]')
 
 # Get the number of ADC Channels
-num_adc_channels = andor.GetNumberADChannels()[1]
+num_adc_channels = andor.GetNumberADChannels()[1] - 1
+print(f'Number of ADC Channels: {num_adc_channels}')
 
-# Vertical Shift Speeds
-num_vss = andor.GetNumberVSSpeeds()[1]
+# Number of Available Vertical Shift Speeds
+num_vss = (andor.GetNumberVSSpeeds()[1] - 1)
+print(f'Number of Available Vertical Shift Speeds: {num_vss}')
 
-# Horizontal Shift Speeds
+# Get the value of the VSS based on the index
+vs_speeds = []
+print(f'Vertical Shift Speed Values')
+for vss_index in range(num_vss):
+    vs_speeds.append([andor.GetVSSpeed(vss_index)])
+    print(f'Vertical Shift Speed [Index {vss_index}]: {andor.GetVSSpeed(vss_index)} Microseconds Per Pixel Shift')
+
+# Get Number of Available Horizontal Shift Speeds Per AD Channel
+print(f'Number of Horizontal Shift Speeds Per AD Channel')
+for channel in range(num_adc_channels):
+    print()
+
+
+
 
 
 
